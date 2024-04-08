986a9248281da823e7c04f0e349c5a6609f5f6d9
ajika1114 <163846082+ajika1114@users.noreply.github.com>
2024-04-08 10:29:58 +0300

    laba4_new

--- Program.cs
+++ Program.cs
@@ -8,9 +8,16 @@ class Program
     {
         List<Person> people = new List<Person>
         {
-            new Person { FirstName = "Іван", LastName = "Петров", Birthday = new DateTime(2003, 5, 15) },
-            new Person { FirstName = "Марія", LastName = "Сидорова", Birthday = new DateTime(2005, 8, 22) },
-            new Person { FirstName = "Олександр", LastName = "Іванов", Birthday = new DateTime(1999, 12, 31) }
+            new Person { FirstName = "Леся", LastName = "Українка", Birthday = new DateTime(2005, 2, 25) },
+            new Person { FirstName = "Тарас", LastName = "Шевченко", Birthday = new DateTime(1814, 3, 9) },
+            new Person { FirstName = "Іван", LastName = "Франко", Birthday = new DateTime(2001, 8, 27) },
+            new Person { FirstName = "Леонід", LastName = "Каденюк", Birthday = new DateTime(1951, 1, 28) },
+            new Person { FirstName = "Сергій", LastName = "Корольов", Birthday = new DateTime(2007, 1, 12) },
+            new Person { FirstName = "Василь", LastName = "Стус", Birthday = new DateTime(1938, 1, 8) },
+            new Person { FirstName = "Леонід", LastName = "Брежнєв", Birthday = new DateTime(2006, 12, 19) },
+            new Person { FirstName = "Анатолій", LastName = "Солов'яненко", Birthday = new DateTime(1932, 9, 8) },
+            new Person { FirstName = "Леся", LastName = "Курбас", Birthday = new DateTime(2023, 1, 25) },
+            new Person { FirstName = "Пантелеймон", LastName = "Куліш", Birthday = new DateTime(1819, 3, 5) }
         };
 
         var selectedPeople = people.Where(p => p.Birthday.Year > 2000)
